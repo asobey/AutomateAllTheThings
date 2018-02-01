b359d77bfeefcebdf4b9f611e9734aa478de7ce5
asobey <alex.sobey@gmail.com>
2018-02-01 16:53:23 -0600

    Update TwilioTest.py

--- TwilioTest.py
+++ TwilioTest.py
@@ -13,11 +13,11 @@ speed_test = os.system('speedtest-cli --simple')
 print(speed_test)
 
 accountSID = 'AC046802bc1bcd9de48e4a997d35888ee9'
-authToken = 'c31f8249b4d5c15fa87edeb332ac01f7'
+authToken = ''
 
 client = Client(accountSID, authToken)
 
 myTwilioNumber = '+15129569204'
-myCellPhone = '+15129568288'
+myCellPhone = '+1'
 
 client.messages.create(body=speed_test, from_=myTwilioNumber, to=myCellPhone)
