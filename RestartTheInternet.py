9e52292fe0617f57c96abe9cf9ca764962bef062
asobey <alex.sobey@gmail.com>
2018-04-15 16:05:51 -0500

    first commit of this .py

---
+++ RestartTheInternet.py
@@ -0,0 +1,44 @@
+#! python3
+# RestartTheInterent.py - Xfininity new modem signal, Restart Router
+
+import requests, webbrowser, bs4, time
+from selenium import webdriver
+
+#webbrowser.open('https://customer.xfinity.com/#/services/modem-restart')
+browser = webdriver.Firefox()
+#browser.get('https://customer.xfinity.com/#/services/modem-restart')
+browser.get('https://login.xfinity.com/login?r=comcast.net&s=oauth&continue=https%3A%2F%2Foauth.xfinity.com%2Foauth%2Fauthorize%3Fclient_id%3Dmy-account-web%26prompt%3Dlogin%26redirect_uri%3Dhttps%253A%252F%252Fcustomer.xfinity.com%252Foauth%252Fcallback%26response_type%3Dcode%26state%3D%2523%252Fservices%252Fmodem-restart%26response%3D1&forceAuthn=1&client_id=my-account-web&reqId=4de868f6-4297-41f7-9a6a-89cc73e6dc1f')
+
+### Modem Reboot
+browser2 = webdriver.Firefox()
+browser2.get('http://192.168.0.1/AMSOHJDAMKGLGPNC/userRpm/Index.htm')
+
+time.sleep(2)
+userName = browser.find_element_by_id('userName')
+userName.send_keys('tp-link')
+
+pcPassword = browser.find_element_by_id('pcPassword')
+pcPassword.send_keys('Thri11H0usetp')
+
+time.sleep(2)
+
+username = browser.find_element_by_id('user')
+username.send_keys('alex.sobey@gmail.com')
+
+passwd = browser.find_element_by_id('passwd')
+passwd.send_keys('Thri11H0usexf')
+
+passwd.submit()
+
+time.sleep(10)
+
+browser.get('https://customer.xfinity.com/#/services/modem-restart')
+
+
+### Modem Reboot
+#browser2 = webdriver.Firefox()
+#browser2.get('http://192.168.0.1/AMSOHJDAMKGLGPNC/userRpm/Index.htm')
+
+
+
+pcPassword.submit
