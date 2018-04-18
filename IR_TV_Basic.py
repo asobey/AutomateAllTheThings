f12116b0de2bb81b173e3210279c11239adc596a
asobey <alex.sobey@gmail.com>
2018-04-18 03:09:17 -0500

    aded IR_TV_Basic and MQTT_Client_Demo

---
+++ IR_TV_Basic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+#Call program from SSH with: 'python /home/pi/Desktop/IR_TV_Basic.py'
+#resource: http://www.piddlerintheroot.com/ir-blaster-lirc/
+# https://www.youtube.com/watch?v=7vmzQ8bWwmo
+# Rpi3 Model B Pinouts: https://www.jameco.com/Jameco/workshop/circuitnotes/raspberry-pi-circuit-note.html
+
+import os, time
+
+#Turn On TV and change input source
+os.system('irsend SEND_ONCE /home/pi/lircd.conf KEY_POWER')
+time.sleep(12)
+os.system('irsend SEND_ONCE /home/pi/lircd.conf KEY_I')
+time.sleep(1)
+os.system('irsend SEND_ONCE /home/pi/lircd.conf KEY_5')
+time.sleep(1)
+os.system('irsend SEND_ONCE /home/pi/lircd.conf KEY_ENTER')
