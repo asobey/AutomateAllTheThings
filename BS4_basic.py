1c7e153dccb2c431b1645594d53c9a5920ff836a
Sobey, Alexander R 362980016 <alex.r.sobey@nasa.gov>
2018-05-22 12:15:05 -0500

    Start of file

---
+++ BS4_basic.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+
+import requests
+
+#url = input("Enter a website to extract the URL's from: ")
+url = 'http://www.pythonforbeginners.com'
+
+#r = requests.get('http://'+ url)
+r = requests.get(url)
+
+data = r.text
+
+soup = BeautifulSoup(data)
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
